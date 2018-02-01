589f59517d96688bd0c6a0a30cfd1537ad8d89d8
ramirezl <ramirezl@zippyttech.com>
2018-02-01 10:24:11 -0400

    fkjsnfsd

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+var express = require('express');
+var app = express();
 
-//Install express server
-const express = require('express');
-const app = express();
+app.set('port', (process.env.PORT || 5000));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// views is directory for all template files
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
