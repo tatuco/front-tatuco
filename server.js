0938cefda17abcce2a5afafce7f855a59ce4ef0a
ramirezl <ramirezl@zippyttech.com>
2018-02-01 13:19:09 -0400

    das

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const api = require('./serve/routes/api');
 
 cons app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/src')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/src'));
